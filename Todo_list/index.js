f49d7ea4ed7ebf10383a475ffda059f875612740
kiranbawa05 <kiranbawa4321@gmail.com>
2026-04-12 01:19:15 +0530

    3rd commit

---
+++ Todo_list/index.js
@@ -0,0 +1,29 @@
+//document.getElementById("button").addEventListener("click",add);
+const add=()=>{
+    let input=document.getElementById("text"); //got the text
+    let item=input.value;
+
+    if (item=="") return; //base case
+
+    let time=document.getElementById("time").value; //got time value
+    let timestamp=document.createElement("small");
+    timestamp.textContent=time;
+    
+
+    let liB=document.createElement("button"); //created button
+    liB.textContent="delete"; //named button
+    liB.onclick=function(){
+        li.remove();
+    };
+
+    let li=document.createElement("li");  //created a list item
+    li.textContent=item;
+    li.appendChild(timestamp);
+    li.appendChild(liB);//added button to list item
+
+    document.getElementById("list").appendChild(li);
+
+    input.value="";
+
+    
+}
